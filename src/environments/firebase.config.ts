03a5d1ca636927770b51a21441cbbbd105f9bcd7
vasudevanr <rakesh.vasudevan@agilysys.com>
2023-10-11 07:13:09 +0530

    commit firebase

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,8 +1,10 @@
 export const FireBaseConfig = {
-  apiKey: "<YOUR_API_KEY>",
-  authDomain: "<YOUR_AUTH_DOMAIN>",
-  databaseURL: "<YOUR_DATABASE_URL>",
-  projectId: "<YOUR_PROJECT_ID>",
-  storageBucket: "<YOUR_STORAGE_BUCKETS>",
-  messagingSenderId: "<YOUR_MESSAGING_SENDER_ID>",
+  apiKey: "AIzaSyBaDDF-mTcPCjMqkVlndq-RBCSKR0OTzMY",
+  authDomain: "creativesbox-cfe2d.firebaseapp.com",
+  databaseURL:
+    "https://creativesbox-cfe2d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "creativesbox-cfe2d",
+  storageBucket: "creativesbox-cfe2d.appspot.com",
+  messagingSenderId: "376348799038",
+  appId: "1:376348799038:web:36d83ded3a2fdf7753216f",
 };
